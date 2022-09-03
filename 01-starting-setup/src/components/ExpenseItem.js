3d0697e8a686a1969865c9289aa33d326391b9c7
Lamyaeltatawy <lamyaeltatawy@gmail.com>
2022-09-03 19:04:27 +0200

    commit2

---
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -0,0 +1,22 @@
+import ExpenseDate from './ExpenseDate';
+import Card from './card';
+import { useCallback } from 'react';
+import './ExpenseItem.css';
+const ExpenseItem=(props)=>{
+    //const ExpenseDate= new Date(2021,2,28);
+    //const Expensetitle='car insurance';
+    //const Expenseprice=224.5;
+const cliclkHandler=()=>{console.log('printed')};    
+
+return(
+   
+ <Card className='expense-item'>
+   <ExpenseDate date={props.date} /> 
+   <div className='expense-item__description'> <h2>{props.title}</h2></div>
+   <div className='expense-item__price'>${props.amount}</div>
+   <button onClick={cliclkHandler}>click</button>
+ </Card>
+);
+
+}
+export default ExpenseItem;
