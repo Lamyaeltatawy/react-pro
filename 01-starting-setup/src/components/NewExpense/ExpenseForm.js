50bc104470c41bdea6d1f9549d8e653f39074ca7
Lamyaeltatawy <lamyaeltatawy@gmail.com>
2022-09-04 21:47:55 +0200

    commit4

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -13,29 +13,51 @@ const ExpenseForm =()=>{
  };
  const dateChangeHandler=(event)=>{
     setEnteredDate(event.target.value);
+   const expenseData={
+    title:enteredTitle,
+    amount:enteredAmount,
+    date:new Date(enteredDate),
+   }; 
+
+console.log(expenseData);   
  };
 
 
-
+const submitHandler=(event)=>{
+    event.preventDefault(); //prevent form to reload after submit
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
+};
 
     return(
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'></div>
                  <label className='new-expense__control label'>title</label>
-                 <input className='new-expense__control input' type='text' onChange={titleChangeHandler} />
+                 
+                 <input className='new-expense__control input'
+                  type='text' 
+                  value={enteredTitle} 
+                  onChange={titleChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
                  <label className='new-expense__control label'>Amount</label>
-                 <input className='new-expense__control input' type='number' min="0.01" step='0.01' onChange={amountChangeHandler}/>
+                  
+                 <input className='new-expense__control input' 
+                 type='number'
+                  value={enteredAmount} 
+                  min="0.01" step='0.01' 
+                  onChange={amountChangeHandler}/>
                 <div/>
                 <div className='new-expense__control'>
                  <label className='new-expense__control label'>date</label>
-                 <input className='new-expense__control input' type='date' min='2019-01-01' max='2022-12-30' onChange={dateChangeHandler}/>
+                 
+                 <input className='new-expense__control input' type='date' value={enteredDate} min='2019-01-01' max='2022-12-30' onChange={dateChangeHandler}/>
                 </div>   
             </div>
             <div className='new-expense__actions'>
-              <button type='submit'>Add Expense</button>
+              <button type='submit' >Add Expense</button>
             </div>
             
         </form>
